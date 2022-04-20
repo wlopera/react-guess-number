ea069bdcb8a3ee0f2a014784ac787004c55e51b6
lopera.william@gmail.com <William>
2022-04-20 16:41:41 -0500

    inicio del proyecto

---
+++ screens/StartGameScreen.js
@@ -0,0 +1,41 @@
+import { StyleSheet, TextInput, View } from "react-native";
+import React from "react";
+import PrimaryButton from "../components/PrimaryButton";
+
+const StartGameScreen = () => {
+  return (
+    <View style={styles.inputContainer}>
+      <TextInput style={styles.numberInput} maxLength={2} />
+      <PrimaryButton>Reiniciar</PrimaryButton>
+      <PrimaryButton>Confirmar</PrimaryButton>
+    </View>
+  );
+};
+
+export default StartGameScreen;
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    padding: 16,
+    marginTop: 100,
+    backgroundColor: "#72063c",
+    marginHorizontal: 24,
+    borderRadius: 8,
+    elevation: 4,
+    shadowColor: "black",
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 6,
+    shadowOpacity: 0.25,
+  },
+
+  numberInput: {
+    height: 50,
+    width: 50,
+    fontSize: 32,
+    borderBottomColor: "#ddb52f",
+    borderBottomWidth: 2,
+    color: "#ddb52f",
+    marginVertical: 6,
+    textAlign: "center",
+  },
+});
