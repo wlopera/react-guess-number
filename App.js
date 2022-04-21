aec098c37913618de2aae64c08bd2725481a8024
lopera.william@gmail.com <William>
2022-04-21 09:00:41 -0500

    cambiop de estilos y fondo de la pantalla

--- App.js
+++ App.js
@@ -1,8 +1,28 @@
-import { StyleSheet } from "react-native";
+import { ImageBackground, StyleSheet } from "react-native";
 import StartGameScreen from "./screens/StartGameScreen";
+import { LinearGradient } from "expo-linear-gradient";
 
 export default function App() {
-  return <StartGameScreen />;
+  return (
+    <LinearGradient colors={["#ddb52f", "#4e0329"]} style={styles.screen}>
+      <ImageBackground
+        source={require("./assets/images/background.png")}
+        resizeMode="cover"
+        style={styles.screen}
+        imageStyle={styles.backgroundImage}
+      >
+        <StartGameScreen />
+      </ImageBackground>
+    </LinearGradient>
+  );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+  },
+
+  backgroundImage: {
+    opacity: 0.15,
+  },
+});
